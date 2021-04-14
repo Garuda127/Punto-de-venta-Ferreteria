38b04a6d17fe91d591e1874fd03a0a076cffca1d
hecto <hecto@LENOVO>
2021-04-14 17:14:03 -0500

    Se implemento interfaz de menu

--- punto de venta/Menu.cs
+++ punto de venta/Menu.cs
@@ -16,5 +16,10 @@ namespace punto_de_venta
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
