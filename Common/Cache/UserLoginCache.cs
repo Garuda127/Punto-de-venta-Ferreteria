3a0e5cb19811a9c9e88108fb3fab964defd63a9c
Garuda127 <71472029+Garuda127@users.noreply.github.com>
2021-04-21 13:53:56 -0500

    Login terminado
    se agrego las buenas practicas de programacion y se ha conectado con la base de datos

---
+++ Common/Cache/UserLoginCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Cache
+{
+    public static class UserLoginCache
+    {
+        public static int IdUser { get; set; }
+        public static string User { get; set; }
+        public static string Password { get; set; }
+    }
+}
--- punto de venta/Program.cs
+++ punto de venta/Program.cs
@@ -16,7 +16,7 @@ namespace punto_de_venta
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Productos());
+            Application.Run(new Login());
         }
     }
 }
