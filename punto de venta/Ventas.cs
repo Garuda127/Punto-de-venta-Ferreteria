b5f5c03ec24922bfe3bda7a0fcd6a10511f71347
HugoLuci <72964601+HugoLuci@users.noreply.github.com>
2021-05-27 16:32:45 -0500

    Reportes
    Hacer reporte de venta

--- punto de venta/Ventas.cs
+++ punto de venta/Ventas.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace punto_de_venta
 {
@@ -120,7 +121,35 @@ namespace punto_de_venta
                 MessageBox.Show("No seleccio nada", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
+        int cctxt = 0;
+        string path = "";
+        public void generarreporte()
+        {if(path == "")
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        path = fbd.SelectedPath + "\\";
+                    }
+                }
+            }
+            
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path+"R"+cctxt+".txt"))
+            {
+                file.WriteLine("Ferreteria X");
+                file.WriteLine("==================");
+                for (int i =0;i<cc;i++)
+                {
+                    file.WriteLine("Articulo: "+mat[0,i]+"== Cantidad: "+mat[2,i]+"== Valor: "+mat[1,i]);
+                }
+                file.WriteLine("==================");
+                file.WriteLine("Total: " + calcularTotal());
+                
+            }
+            cctxt++;
+        }
         private void button4_Click(object sender, EventArgs e)
         {
             string nombre = "";
@@ -134,6 +163,7 @@ namespace punto_de_venta
             }
             if (nombre != "")
             {
+                
                 dataGridView1.Rows.Clear();
                 for (int i = 0; i < cc; i++)
                 {
@@ -187,12 +217,12 @@ namespace punto_de_venta
             double total = calcularTotal();
             DateTime myDateTime = DateTime.Now;
             string fecha = myDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
-                   
-                Random rnd = new Random();
+            Random rnd = new Random();
             int id = rnd.Next(1, 10000);
             VentaDao.VentaDAO vt = new VentaDao.VentaDAO();
             if (MessageBox.Show("Desea realizar su compra", "Confirma en Acceptar", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
+                generarreporte();
                 vt.Reg(id, fecha, total);
            }
             for(int i=0;i < cc; i++)
